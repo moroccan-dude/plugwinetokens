2c27b70b544248332cb07de84589b809df6c63fc
mehdi <mehdibennani@hotmail.com>
2014-09-18 16:00:18 +0000

    hbm2ddl class

---
+++ src/main/java/com/plugwine/util/Hbm2DDL.java
@@ -0,0 +1,49 @@
+package com.plugwine.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+
+public class Hbm2DDL {
+
+	    public static void main(String[] args) throws IOException {
+	        execute(args[0], args[1], args[2], Boolean.parseBoolean(args[3]), Boolean.parseBoolean(args[4]), Boolean.parseBoolean(args[5]));
+	    }
+	     
+//	public static void main(String[] args)
+//	{
+//     String hbmCfgFilePath = "M:\\projects\\java-projects\\PlugwineTokens\\src\\main\\resources\\hibernate\\hibernate.cfg.xml";
+//	   String propFilePath = "M:\\projects\\java-projects\\PlugwineTokens\\src\\main\\filters\\environment-dev_mssql.properties";
+//	   String destinationPath = "M:\\projects\\java-projects\\PlugwineTokens\\src\\main\\resources\\hibernate\\a.ddl";
+//		try {
+//			execute(hbmCfgFile,propFileName,destination,true,true);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
+	    public static void execute(String hbmCfgFilePath, String propFilePath, String destinationPath, boolean create, boolean format, boolean export) throws FileNotFoundException, IOException 
+	    {
+	        System.out.println("Starting schema export using: "+ hbmCfgFilePath + " and " + propFilePath);
+	        if(export)
+	        	System.out.println(">>>>>>>>>>>>>>>>>>   Exporting the SQL   <<<<<<<<<<<<<<<<<<");
+	        Configuration config = new Configuration();
+	        config.configure(new File(hbmCfgFilePath));
+
+	        Properties properties = new Properties();
+	        properties.load(new FileInputStream(new File(propFilePath)));
+	        config.addProperties(properties);
+
+	        SchemaExport schemaExport = new SchemaExport(config);
+	        schemaExport.setOutputFile(destinationPath);
+	        schemaExport.setFormat(format);
+	        schemaExport.setDelimiter(";");
+
+	        schemaExport.execute(true, export, true, create);
+	        System.out.println("Schema exported to " + destinationPath);
+	    }
+}
