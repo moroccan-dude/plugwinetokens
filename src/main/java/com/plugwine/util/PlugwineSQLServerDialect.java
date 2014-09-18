41c3d2dce06aa7c131f127ec89e8bbc13f2eda0e
mehdi <mehdibennani@hotmail.com>
2014-09-18 09:04:01 +0000

    added comments

--- src/main/java/com/plugwine/util/PlugwineSQLServerDialect.java
+++ src/main/java/com/plugwine/util/PlugwineSQLServerDialect.java
@@ -11,23 +11,24 @@ public class PlugwineSQLServerDialect extends SQLServer2008Dialect{
 	
 	 public PlugwineSQLServerDialect() {
 		    super();
-		    //registerHibernateType(Types.VARCHAR, name);
-		    //registerColumnType(Types.VARCHAR, "lvarchar($l)");
-		   
 		    registerColumnType(Types.CHAR, "nchar(l)");
 		    registerColumnType(Types.LONGVARCHAR, "nvarchar($l)");
 		    registerColumnType(Types.CLOB, "ntext");
 		    
 		    registerHibernateType( Types.NVARCHAR, 255, StandardBasicTypes.STRING.getName() );
 		    //registerColumnType( Types.VARCHAR, MAX_LENGTH, "varchar($l)" );
-		    // Found: nvarchar, expected: varchar(255)  
-		    // nvarchar in DB metadata, varchar(255) in the mapping?
+		    // Adding the following line fixed the hibernate exception saying:
+		    // Found: nvarchar, expected: varchar(255)
+		    // Which really means:
+		    // found nvarchar in DB metadata and found varchar(255) in the mapping?
 		    // so I added the following line
 		    registerColumnType(Types.VARCHAR, 255, "nvarchar($l)");
 		    
+		    // I also had to implement the XML user type
 		    registerHibernateType(MSSQLXMLUserType.XML_SQL_TYPE, "xmltype");
 	        registerColumnType(MSSQLXMLUserType.XML_SQL_TYPE, "xmltype");
 	        
+	        // I also had to implement the Uniqueidentifier user type
 	        registerHibernateType(MSSQLUUIDUserType.UUID_SQL_TYPE, "uniqueidentifier");
 	        registerColumnType(MSSQLUUIDUserType.UUID_SQL_TYPE, "uniqueidentifier");
 	 }
